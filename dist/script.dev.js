64cf085227d40566e569c580a9545c2ba6cbae11
RACHNA PANDITA <rachna.pandita@db.com>
2021-10-29 10:39:18 +0530

    Added logic for calculating total score

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -11,8 +11,7 @@ console.log(ansRow); // const submitBtn = document.getElementById('submit');
 var skipBtn = document.getElementById('skip');
 console.log(skipBtn);
 var count = 0;
-var duration = 0; // let currentQuiz = 0;
-
+var duration = 0;
 var scoreCount = 0;
 skipBtn.addEventListener('click', function () {
   step();
@@ -28,7 +27,7 @@ var step = function step() {
 
   qaSet[count].className = 'quiz_header active';
 
-  if (count == 4) {
+  if (count == 5) {
     skipBtn.style.display = 'none';
     clearInterval(durationTime);
     countdown.innerHTML = 0;
@@ -46,11 +45,14 @@ ansRow.forEach(function (ansRowSingle) {
     if (valid == "valid") {
       scoreCount += 10;
       score.innerHTML = scoreCount;
-      totalScore.innerHTML = score;
+      console.log("******");
+      console.log(score);
+      totalScore.innerHTML = scoreCount;
+      console.log(totalScore);
     } else {
       scoreCount = scoreCount;
       score.innerHTML = scoreCount;
-      totalScore.innerHTML = score;
+      totalScore.innerHTML = scoreCount;
     }
   });
 });
@@ -65,44 +67,4 @@ var durationTime = setInterval(function () {
   if (countdown.innerHTML == "10") {
     step();
   }
-}, 1000); // loadQuiz()
-// function loadQuiz() {
-//     deselectAnswers()
-//     const currentQuizData = quizData[currentQuiz]
-//     questionEl.innerText = currentQuizData.question
-//     a_text.innerText = currentQuizData.a
-//     b_text.innerText = currentQuizData.b
-//     c_text.innerText = currentQuizData.c
-//     d_text.innerText = currentQuizData.d
-// }
-// function deselectAnswers() {
-//     answerEls.forEach(answerEl => answerEl.checked = false)
-// }
-// const getSelected = () => {
-//     let answer
-//     answerEls.forEach(answerEl => {
-//         if(answerEl.checked) {
-//             answer = answerEl.id
-//         }
-//     })
-//     return answer
-// }
-// submitBtn.addEventListener('click', () => {
-//     const answer = getSelected()
-//     if(answer) {
-//        if(answer === quizData[currentQuiz].correct) {
-//            score++;
-//            score.value=score;
-//         //    console.log(score);
-//        }
-//        currentQuiz++
-//        if(currentQuiz < quizData.length) {
-//            loadQuiz()
-//        } else {
-//            quiz.innerHTML = `
-//            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-//            <button onclick="location.reload()">Reload</button>
-//            `
-//        }
-//     }
-// })
+}, 1000);
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quiz = document.getElementById('quiz');
 let score = document.getElementById('score');
 const countdown = document.getElementById('countdown');
-let totalScore = document.getElementById('totalScore')
+let totalScore = document.getElementById('totalScore');
 let qaSet = document.querySelectorAll('.quiz_header');
 let ansRow = document.querySelectorAll('.quiz_header .qa_ans_row input');
 console.log(ansRow);
@@ -11,7 +11,6 @@ console.log(skipBtn);
 
 let count = 0;
 let duration = 0;  
-// let currentQuiz = 0;
 let scoreCount = 0;
 
 skipBtn.addEventListener('click',() => {
@@ -25,7 +24,7 @@ const step = () => {
         qaSet[i].className = 'quiz_header';
     }
       qaSet[count].className = 'quiz_header active';
-      if(count == 4) {
+      if(count == 5) {
           skipBtn.style.display='none';
           clearInterval(durationTime);
           countdown.innerHTML = 0;
@@ -44,17 +43,21 @@ ansRow.forEach((ansRowSingle) => {
         if(valid == "valid") {
             scoreCount += 10;
             score.innerHTML = scoreCount;
-            totalScore.innerHTML = score;
+            console.log("******");
+            console.log(score);
+            totalScore.innerHTML = scoreCount;
+            console.log(totalScore);       
             
         }else{
             scoreCount = scoreCount;
             score.innerHTML = scoreCount;
-            totalScore.innerHTML = score;
+            totalScore.innerHTML = scoreCount;
 
         }
     })
 });
 
+   
 const durationTime = setInterval(() => {
     if(duration == 10) {
         duration = 0;
@@ -64,65 +67,7 @@ const durationTime = setInterval(() => {
      if(countdown.innerHTML == "10"){
          step();
      }
- 
  }, 1000);
 
 
-
-// loadQuiz()
-
-// function loadQuiz() {
-
-//     deselectAnswers()
-
-//     const currentQuizData = quizData[currentQuiz]
-
-//     questionEl.innerText = currentQuizData.question
-//     a_text.innerText = currentQuizData.a
-//     b_text.innerText = currentQuizData.b
-//     c_text.innerText = currentQuizData.c
-//     d_text.innerText = currentQuizData.d
-// }
-
-
-// function deselectAnswers() {
-//     answerEls.forEach(answerEl => answerEl.checked = false)
-// }
-
-// const getSelected = () => {
-//     let answer
-//     answerEls.forEach(answerEl => {
-//         if(answerEl.checked) {
-//             answer = answerEl.id
-//         }
-//     })
-//     return answer
-// }
-
-
-
-
-// submitBtn.addEventListener('click', () => {
-//     const answer = getSelected()
-//     if(answer) {
-//        if(answer === quizData[currentQuiz].correct) {
-//            score++;
-//            score.value=score;
-//         //    console.log(score);
-
-//        }
-
-//        currentQuiz++
-
-//        if(currentQuiz < quizData.length) {
-//            loadQuiz()
-//        } else {
-//            quiz.innerHTML = `
-//            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-//            <button onclick="location.reload()">Reload</button>
-//            `
-//        }
-//     }
-// })
 
